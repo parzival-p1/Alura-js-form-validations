b6b20a5f1d5ace5b950e7db367dc0994113b5d3f
Paco Hdez <francj.hdez@gmail.com>
2023-04-04 20:49:03 -0600

    final commit for form validation

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+import { valida } from "./validations.js";
+
+const inputs = document.querySelectorAll("input");
+inputs.forEach( input => {
+    input.addEventListener("blur", (input) => {
+        valida(input.target);
+    })
+});
